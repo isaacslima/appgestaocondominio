850fa47a68536c6a06def1757fd9605ac38ea147
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-05-24 11:30:45 -0300

    add login page

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,19 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <Login/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Login from './components/Login';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Login,
   },
 
   data: () => ({
